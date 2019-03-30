228d2430a48c36e942f871866be61c8d4e86faf8
Hugo <hugomn@gmail.com>
2019-03-30 12:55:14 +0000

    Automated deployment to GitHub Pages: e768dda6e60748dbaa61bd430fe1cd5c85216a9f

---
+++ component---src-pages-404-js-e7044b65fdfeccc0dcd8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{149:function(n,e,t){"use strict";t.r(e);var r=t(7),o=t.n(r),u=t(0),a=t.n(u),i=function(n){function e(){return n.apply(this,arguments)||this}return o()(e,n),e.prototype.render=function(){return a.a.createElement("div",null,a.a.createElement("h1",null,"Page not found"))},e}(a.a.PureComponent);e.default=i}}]);
+//# sourceMappingURL=component---src-pages-404-js-e7044b65fdfeccc0dcd8.js.map
