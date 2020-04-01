f31e1725fb68e2088b0f7b9d7ecac54edb5a37c2
Hugo Nogueira <hugomn@gmail.com>
2020-04-01 08:54:27 +0200

    Align announcement left

--- src/components/pages/Announcement.js
+++ src/components/pages/Announcement.js
@@ -22,7 +22,7 @@ const Announcement = () => {
 const Subtitle = styled.p`
   color: ${({ dark, theme }) => (dark ? theme.colors.white : theme.colors.black)};
   font-size: ${({ theme }) => theme.scale(0.275)};
-  text-align: justify;
+  text-align: left;
   max-width: 700px;
   padding: 0px 26px;
   margin: 20px auto 0px;
